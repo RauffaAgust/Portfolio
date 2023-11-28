b9849be8c4af1c3491683e98c90936ee50669042
Rauffa Agust <quebreiocontrole@gmail.com>
2023-11-28 11:50:00 -0300

    update

--- Site4/javinha.js
+++ Site4/javinha.js
@@ -3,10 +3,5 @@ function clickMenu() {
         itensMenu.style.display = 'none'
     } else {
         itensMenu.style.display = 'block'
-    } 
-    if (iconMenu.style.borderRadius == '0px 13px 0px 0px') {
-        iconMenu.style.borderRadius = '0px'
-    } else {
-        iconMenu.style.borderRadius = '0px 13px 0px 0px'
     }
 }
