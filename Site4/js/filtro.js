536686e5812d194fe6d6b971634241e3b22f5441
Rauffa Agust <quebreiocontrole@gmail.com>
2023-12-08 21:09:31 -0300

    update

---
+++ Site4/js/filtro.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const filter = document.querySelectorAll('.filter');
+    const item = document.querySelectorAll('.item');
+
+    filter.forEach(button => {
+        
+    })
+} )
